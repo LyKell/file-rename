05e823af02d3b020cdb108c84605adbfafc3385b
LyKell <kev_dev@hotmail.com>
2020-12-18 19:48:10 +0100

    Add docstring

--- src/File.py
+++ src/File.py
@@ -7,6 +7,12 @@ import hashlib
 import shutil
 
 def open_file(folder, filename):
+    """Opens a file and calculates its hashcode.
+
+    Args:
+        folder (str): the source folder of the files to rename.
+        filename (str): the file name.
+    """
     path = os.path.join(folder, filename)
     _, extension = os.path.splitext(path)
 
@@ -17,12 +23,25 @@ def open_file(folder, filename):
 
 
 def retrieve_file(folder=os.getcwd()):
+    """Runs through all files from specified folder.
+
+    Args:
+        folder (str, optional): the source folder of the files to rename. Defaults to current dir.
+    """
     for filename in os.listdir(folder):
         open_file(folder, filename)
 
 
-def rename_file(f, new_name, extension, folder):
-    new_file = os.path.join(folder, new_name + extension)
+def rename_file(f, name, extension, folder):
+    """Renames files and removes it if duplicate.
+
+    Args:
+        f (str): the file path
+        name (str): the name to rename to
+        extension (str): the original file extension
+        folder (str): the destination folder of the new file (same as source folder)
+    """
+    new_file = os.path.join(folder, name + extension)
     try:
         os.rename(f, new_file)
     except FileExistsError:
