24b03313936a46b19e60269d7a662a9bbfdb2e5d
LyKell <kev_dev@hotmail.com>
2020-12-18 19:32:08 +0100

    Add file renaming

--- src/File.py
+++ src/File.py
@@ -3,14 +3,24 @@
 
 import os
 import sys
+import hashlib
+import shutil
 
-def open_file(filename, folder):
-    with open(os.path.join(folder, filename), 'r') as f:
-        print(f.name)
+def open_file(folder, filename):
+    path = os.path.join(folder, filename)
+    _, extension = os.path.splitext(path)
+
+    f = open(path, 'rb').read()
+    hash_code = hashlib.md5(f).hexdigest()
+
+    rename_file(path, hash_code, extension, folder)
 
 
 def retrieve_file(folder=os.getcwd()):
     for filename in os.listdir(folder):
-        open_file(filename, folder)
+        open_file(folder, filename)
 
 
+def rename_file(f, new_name, extension, folder):
+    new_file = os.path.join(folder, new_name + extension)
+    os.rename(f, new_file)
